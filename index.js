e8532501524166e6a283aa07942a06740a67c761
rukonit <rukon-it@live.com>
2021-08-11 22:52:02 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.length('/home', (req, res) => {
+    
+})
+
+app.listen(3000, ()=> {
+    console.log("LISTENING ON PORT 3000");
+})
